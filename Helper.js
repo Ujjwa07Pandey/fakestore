fb2cb623e097af6f1a96d764c798892dd8a18a06
Leonidus300 <leonidus880@gmail.com>
2021-02-02 21:15:58 +0530

    BoilerPlate Setup

--- Helper.js
+++ Helper.js
@@ -2,5 +2,5 @@ export const MAXWIDTH = 400;
 export const MAXHEIGHT = 640;
 export const BIGFONT = 0.8;
 export const CUSTOMFONT = (size) => {
-    return width > MAXWIDTH ? moderateScale(size) * BIGFONT : 
+    return width > MAXWIDTH ? moderateScale(size) * BIGFONT : moderateScale(size);
 }
